1c97604c0d94e4ca230310c716852f0538c40452
KevRWB <kevinrenaultoff@gmail.com>
2022-05-05 16:23:38 +0200

    Star the real story

--- src/main/kotlin/Hero.kt
+++ src/main/kotlin/Hero.kt
@@ -1,4 +1,4 @@
-class Hero (val name:String, val health: Int, val att:Int, val def:Int, val initiative:Int, val story:String) {
+class Hero (val id: Int, val name:String, val health: Int, val att:Int, val def:Int, val initiative:Int, val story:String) {
     fun whoAmI(){
         println("Heey, I'm $name. My stats are:\nHealth: $health, attack: $att, defense: $def, initiative: $initiative.")
         println("Here's my story:\n$story")
--- src/main/kotlin/Story.kt
+++ src/main/kotlin/Story.kt
@@ -1,41 +1,52 @@
 //  The Main Story
-fun main() {
 
-    initStory()
+import kotlin.system.exitProcess
+
+fun main() {
+    initHeroes()
+    initMonsters()
+    while(true){
+        initStory()
+        println(heroesList[1].whoAmI())
+    }
+
+
 
 }
-
-
 var heroesList = mutableListOf<Hero>()
 fun initHeroes(){
-    var hector = Hero("Hector", 17, 15, 18, 12, "I'm Hector, son of Priam")
+    var hector = Hero(1,"Hector", 17, 15, 18, 12, "I'm Hector, son of Priam")
     heroesList.add(hector)
-    var irina = Hero("Irina", 11, 17, 15, 20, "My name is Irina" )
+    var irina = Hero(2,"Irina", 11, 17, 15, 20, "My name is Irina" )
     heroesList.add(irina)
 }
 
 fun showHeroes(list: List<Hero>){
+    println("It's time to chose a hero in the list :")
     for(hero in list){
-        println(hero.story)
     }
 }
+var monstersList = mutableListOf<Monster>()
+fun initMonsters(){
+    var zombie = Monster(1, "Zombie", 12, 6, 8, 2, "A classic Zombie")
+    monstersList.add(zombie)
+    var wolf = Monster(2, "Wolf", 6, 6, 4, 10, "A lambda wolf from the woods")
+    monstersList.add(wolf)
+}
 
-var goOn = false
 fun initStory(){
     println("Hello, welcome in this epic battle program, do you want to begin ?")
     println("O:N")
     var beginAnswer = readln().lowercase()
     if(beginAnswer == "o" || beginAnswer == "n"){
-        println("Let's go !!!")
-        goOn = true
+        if(beginAnswer == "o"){
+            println("Let's go !!!")
+        } else {
+            println("As you like... Bye")
+            exitProcess(1)
+        }
     } else {
-        println("It's your choice, as you like...")
+        println("Don't understand your answer, please enter O or N")
     }
 
-}
-
-fun testGoOn(){
-    if(goOn == true){
-
-    }
 }
